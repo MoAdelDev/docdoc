122e6684364d4f50cb8121689fd99728e072ff9f
MoAdelDev <m.adeldev10@gmail.com>
2024-01-20 00:46:08 +0200

    Remove unused elements

--- lib/features/login/data/models/login_response.g.dart
+++ lib/features/login/data/models/login_response.g.dart
@@ -16,20 +16,7 @@ LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
           : UserData.fromJson(json['data'] as Map<String, dynamic>),
     );
 
-Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
-    <String, dynamic>{
-      'message': instance.message,
-      'code': instance.code,
-      'status': instance.status,
-      'data': instance.userData,
-    };
-
 UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
       json['token'] as String,
       json['username'] as String,
     );
-
-Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
-      'token': instance.token,
-      'username': instance.username,
-    };

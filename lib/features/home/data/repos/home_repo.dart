0a8373be44a0626e686396257c8516d090e6e17d
MoAdelDev <m.adeldev10@gmail.com>
2024-01-21 12:29:09 +0200

    Added home cubit and states

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -8,9 +8,11 @@ class HomeRepo {
 
   HomeRepo(this._apiService);
 
-  Future<ApiResult<HomeResponse>> getHomeDataData(
-      {required String token}) async {
+  Future<ApiResult<HomeResponse>> getHomeDataData() async {
     try {
+      const String token =
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZjYXJlLmludGVncmF0aW9uMjUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNzA1ODMxMDk1LCJleHAiOjE3MDU4MzQ2OTUsIm5iZiI6MTcwNTgzMTA5NSwianRpIjoidUFVb0toaEUzODhTVjhtWiIsInN1YiI6IjQ0NiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.MJ2DApSCGo7x4zITQddUw-cnNKEuHnagX1JINyXvGHo';
+
       final result = await _apiService.getHomeData('Bearer $token');
       return ApiResult.success(result);
     } catch (e) {
---
+++ lib/features/home/logic/cubit/home_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_state.freezed.dart';
+
+@freezed
+class HomeState<T> with _$HomeState<T> {
+  const factory HomeState.initial() = _Initial;
+  const factory HomeState.dashboardLoading() = DashboardLoading;
+  const factory HomeState.dashboardSuccess(T data) = DashboardSuccess<T>;
+  const factory HomeState.dashboardError({required String error}) =
+      DashboardError<T>;
+}
